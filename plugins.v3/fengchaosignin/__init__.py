272ca8a69dad085f101a1e106ce2ddc89f533ec9
madrays <87717138@qq.com>
2026-09-14 22:58:35 +0800

    fix: 修正蜂巢 V3 版本并使用源码安装

--- plugins.v3/fengchaosignin/__init__.py
+++ plugins.v3/fengchaosignin/__init__.py
@@ -189,7 +189,7 @@ class FengchaoSignin(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/madrays/MoviePilot-Plugins/main/icons/fengchao.png"
     # 插件版本
-    plugin_version = "3.0.0"
+    plugin_version = "3.1.6"
     # 插件作者
     plugin_author = "madrays"
     # 作者主页
